3d140fb7fc3be9afd9c4ddbb9d9c210009058aec
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2019-08-27 19:46:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
-index = open('index.html', 'r')
+f = open('index.html', 'r')
+index = f.read()
+f.close()
 
+
+print(index)
 @app.route("/")
 def hello():
-    return "Yo Fam"
+    return index
