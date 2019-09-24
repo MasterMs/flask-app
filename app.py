4d10f1f4965766bf8dd4e34280739af77c234056
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2019-09-24 12:34:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
-import util
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#Routing 
 @app.route("/")
 def index():
-    return util.readAssign(file='index.html')
+    return render_template('index.html',title='Land',name='Marco')
+
+@app.route("/home")
+def home():
+    return render_template('index.html', title='Home', name='Home')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
 
